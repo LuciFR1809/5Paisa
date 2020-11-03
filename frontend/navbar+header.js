57701fad428d0b0b169896b3dd2362ca3a398231
LuciFR1809 <66794738+LuciFR1809@users.noreply.github.com>
2020-11-03 19:45:11 +0530

    Add files via upload

---
+++ frontend/navbar+header.js
@@ -0,0 +1,30 @@
+function smallMenu() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+    document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function showFn() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
